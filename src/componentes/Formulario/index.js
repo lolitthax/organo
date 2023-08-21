c4a3038e6c3370b672f3340271f42a521e8585e6
Aline Damião <aline.isilvadamiao@gmail.com>
2023-08-21 13:37:06 -0300

    refatorando código duplicado

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -6,16 +6,6 @@ import './Formulario.css'
 
 const Formulario = (props) => {
 
-    const times = [
-        'Anemo',
-        'Cryo',
-        'Dendro',
-        'Electro',
-        'Geo',
-        'Hydro',
-        'Pyro'
-    ]
-
     const [nome, setNome] = useState('')
     const [cargo, setCargo] = useState('')
     const [imagem, setImagem] = useState('')
@@ -58,7 +48,7 @@ const Formulario = (props) => {
                 <ListaSuspensa
                     obrigatorio={true}
                     label="Time" 
-                    itens={times}
+                    itens={props.times}
                     valor={time}
                     aoAlterado={valor => setTime(valor)}
                 />
