ed9a5543fecbc36034201655d1ed8c36efc4c257
Aline Damião <aline.isilvadamiao@gmail.com>
2023-08-21 15:10:23 -0300

    corrigindo erros

--- src/componentes/Time/index.js
+++ src/componentes/Time/index.js
@@ -8,7 +8,9 @@ const Time = (props) => {
             <h3 style={{ borderColor: props.corPrimaria }}>{props.nome}</h3>
             <div className='colaboradores'>
                 {props.colaboradores.map(colaborador =>
-                    <Colaborador nome={colaborador.nome}
+                    <Colaborador corDeFundo={props.corPrimaria}
+                        key={colaborador.nome}
+                        nome={colaborador.nome}
                         cargo={colaborador.cargo}
                         imagem={colaborador.imagem}
                     />)}
