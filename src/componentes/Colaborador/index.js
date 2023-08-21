ffb2deecd4e9154e5ea609cc0419681c37fdd61d
Aline Damião <aline.isilvadamiao@gmail.com>
2023-08-21 14:03:20 -0300

    card de colaborador

---
+++ src/componentes/Colaborador/index.js
@@ -0,0 +1,17 @@
+import './Colaborador.css'
+
+const Colaborador = () => {
+    return (
+        <div className='colaborador'>
+            <div className='cabecalho'>
+                <img src='https://github.com/lolitthax.png' alt='' />
+            </div>
+
+            <div className='rodape'>
+                <h4>Aline Damião</h4>
+                <h5>Main DPS</h5>
+            </div>
+        </div>
+    )
+}
+export default Colaborador
